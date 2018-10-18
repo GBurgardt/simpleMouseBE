2b3a8c8a3985c0e2ce518a5fd4db72c7be9b9fb8
German Burgardt <germanandresburgardt@gmail.com>
2018-10-18 02:20:04 -0300

    fafafa

--- server.js
+++ server.js
@@ -1,36 +1,43 @@
 var express = require('express');
 var app = express();
-
 const robot = require("robotjs");
 
+//////////////////////////////////////
+////////// Configuraciones ///////////
+//////////////////////////////////////
+const cors = require('cors')
+app.use(cors());
+//////////////////////////////////////
+//////// Fin Configuraciones /////////
+//////////////////////////////////////
+
+
 app.get('/', function (req, res) {
-    res.send('Hello World!');
+    res.render('index', {
+        title: 'Hey',
+        message: 'Helasjdo'
+    })
 });
 
-app.get('/mouse/move/:direction', function (req, res) {
+app.get('/mouse/move/:deltaX/:deltaY', function (req, res) {
     
     const mousePosi = robot.getMousePos();
 
     // Speed up the mouse.
-    robot.setMouseDelay(2);
+    // robot.setMouseDelay(1);
 
-    const direction = req.params.direction;
+    const deltaX = Number(req.params.deltaX);
+    const deltaY = Number(req.params.deltaY);
 
     robot.moveMouse(
-        mousePosi.x + (
-            (direction === 'right') ? 15 :
-            (direction === 'left') ? -15 :
-            0 
-        ),
-        mousePosi.y + (
-
-            (direction === 'down') ? 15 :
-            (direction === 'up') ? -15 :
-            0
-        ),
+        Number(mousePosi.x + deltaX),
+        Number(mousePosi.y + deltaY)
     );
 
-    res.send(mousePosi)
+    res.send({
+        oldCords: mousePosi,
+        newCords: robot.getMousePos()
+    })
 });
 
 app.get('/mouse/click/:type', function (req, res) {
@@ -43,7 +50,22 @@ app.get('/mouse/click/:type', function (req, res) {
 
 })
 
+
+app.set('views', `${__dirname}/views`);
+
+app.set('view engine', 'pug');
+
+console.log(__dirname);
+
+// require('./build/' + cmd + '.js')
+// path.join(__dirname, 'views/' + viewName)
+
 app.listen(3000, function () {
-  console.log('Listening on port 3000!');
+
+    console.log('Andando')
+
+
 });
+
+
 
